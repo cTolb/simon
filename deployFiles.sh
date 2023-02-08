79e7c4c77c2769f89b760460ac78646abb9b246f
cTolb <ctolbert910@gmail.com>
2023-02-08 15:31:04 -0700

    fixed errors in deployFiles

--- deployFiles.sh
+++ deployFiles.sh
@@ -9,13 +9,13 @@ do
     esac
 done
 
-if [[ -z "$key" || -z "$hsotname" || -z "$service" ]]; then
+if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     printf "\nMissing required parameter.\n"
     printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
     exit 1
 fi
 
-printf "\n----> Deploying files for $serice to $hostname with $key\n"
+printf "\n----> Deploying files for $serivce to $hostname with $key\n"
 
 # Step 1
 printf "n----> Clear out the previous distribution on the target.\n"
