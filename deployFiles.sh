2479dd5a65e54d5859d0a54dbfdfbeb70d0a718f
cTolb <ctolbert910@gmail.com>
2023-03-06 21:15:17 -0700

    fixed deployFiles

--- deployFiles.sh
+++ deployFiles.sh
@@ -15,15 +15,15 @@ if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
     exit 1
 fi
 
-printf "\n----> Deploying files for $serivce to $hostname with $key\n"
+printf "\n----> Deploying files for $service to $hostname with $key\n"
 
 # Step 1
-printf "n----> Clear out the previous distribution on the target.\n"
+printf "\n----> Clear out the previous distribution on the target.\n"
 ssh -i "$key" ubuntu@$hostname << ENDSSH
 rm -rf services/${service}/public
 mkdir -p services/${service}/public
 ENDSSH
 
 # Step 2
-printf "\n----> Copy the distribution package to the tartget.\n"
+printf "\n----> Copy the distribution package to the target.\n"
 scp -r -i "$key" * ubuntu@$hostname:services/$service/public
